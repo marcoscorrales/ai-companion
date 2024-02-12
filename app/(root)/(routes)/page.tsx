0ea6167e25af39f34626ce7f6c52b4352307db3f
marcoscorrales <marcosdecoa@gmail.com>
2024-02-12 11:49:08 +0100

    Companion List

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,16 +1,44 @@
 import { Categories } from "@/components/categories";
-import { SearchInput } from "@/components/search-input"
-import prismadb from "@/lib/prismadb"
+import Companions from "@/components/companion";
+import { SearchInput } from "@/components/search-input";
+import prismadb from "@/lib/prismadb";
 
-const RootPage = async () => {
-    const categories = await prismadb.category.findMany();
+interface RootPageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+}
 
-    return (
-      <div className="h-full p-4 space-y-2">
-        <SearchInput />
-        <Categories data={categories} />
-      </div>
-    )
-  }
-  
-  export default RootPage
+const RootPage = async ({ searchParams }: RootPageProps) => {
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name
+      }
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true
+        }
+      }
+    }
+  });
+
+  const categories = await prismadb.category.findMany();
+
+  return (
+    <div className="h-full p-4 space-y-2">
+      <SearchInput />
+      <Categories data={categories} />
+      <Companions data={data} />
+    </div>
+  );
+};
+
+export default RootPage;
